500f5689d7e4d109365153552c00283bf9a0650a
Vadim Bochkov <vadim.bochkov.96@gmail.com>
2022-12-07 13:48:31 +0600

    The adoptive layout
    Was made the adoptive layout for different devices.

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,28 @@
         }
     };
 }());
+
+const btnBurger = document.querySelector('.header__burger'),
+      btnClose = document.querySelector('.header__nav-close'),
+      menuNav = document.querySelector('.header__nav'),
+      header = document.querySelector('.header');
+
+function openingAndClosingMenus() {
+  btnBurger.addEventListener('click', () => {
+  menuNav.classList.add('header__nav_active');
+  
+  if(menuNav.classList.contains('header__nav_active')) {
+    header.style.opacity = 1;
+  } else {
+    header.style.opacity = 0.80;
+   }
+  });
+  
+  btnClose.addEventListener('click', () => {
+    if(menuNav.classList.contains('header__nav_active')) {
+      menuNav.classList.remove('header__nav_active');
+    }
+  });
+}
+
+openingAndClosingMenus();
