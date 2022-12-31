3843429df8dfbcdca06d5c10d5372571d4e6207b
Vadim Bochkov <vadim.bochkov.96@gmail.com>
2022-12-31 15:01:46 +0600

    Edit files: CSS, JS, HTML
    It was attached registration form and also implemented its opening and closing.

--- JS/main.js
+++ JS/main.js
@@ -10,28 +10,63 @@
 }());
 
 const btnBurger = document.querySelector('.header__burger'),
-      btnClose = document.querySelector('.header__nav-close'),
+      btnCloseNav = document.querySelector('.header__nav-close'),
       menuNav = document.querySelector('.header__nav'),
       header = document.querySelector('.header');
 
-function openingAndClosingMenus() {
+function openingAndClosingMenu() {
   btnBurger.addEventListener('click', () => {
-  menuNav.classList.add('header__nav_active');
-  
-  if(menuNav.classList.contains('header__nav_active')) {
-    header.style.opacity = 1;
-  } else {
-    header.style.opacity = 0.80;
-   }
-  });
-  
-  btnClose.addEventListener('click', () => {
+    menuNav.classList.add('header__nav_active');
+    
     if(menuNav.classList.contains('header__nav_active')) {
-      menuNav.classList.remove('header__nav_active');
+      header.style.opacity = 1;
+    } else {
+      header.style.opacity = 0.80;
     }
+    });
+    
+    btnCloseNav.addEventListener('click', () => {
+      if(menuNav.classList.contains('header__nav_active')) {
+        menuNav.classList.remove('header__nav_active');
+      }
   });
 
   
 }
 
-openingAndClosingMenus();
+openingAndClosingMenu();
+
+const btnCloseForm = document.querySelector('.sign-in__form-close'),
+      form = document.querySelector('.sign-in__form'),
+      wrapperForm = document.querySelector('.form-wrapper'),
+      btnNavForm = document.querySelectorAll('.header__link');
+
+function openingAndClosingForm() {
+  const openAndCloseFormBtn = () => {
+    if(!wrapperForm.classList.contains('form-wrapper_active')) {
+      wrapperForm.classList.add('form-wrapper_active');
+    } else {
+      wrapperForm.classList.remove('form-wrapper_active');
+    }
+  };
+
+  const btnCrossCloseForm = () => {
+    if(wrapperForm.classList.contains('form-wrapper_active')) {
+      wrapperForm.classList.remove('form-wrapper_active');
+    }
+  };
+
+  const closeWithWrapperForm = event => {
+    const target = event.target;
+
+    if (!target.closest('form')) {
+      wrapperForm.classList.remove('form-wrapper_active');
+    }
+  };
+
+  btnNavForm[3].addEventListener('click', openAndCloseFormBtn);
+  btnCloseForm.addEventListener('click', btnCrossCloseForm);
+  wrapperForm.addEventListener('click', closeWithWrapperForm);
+}
+
+openingAndClosingForm();
